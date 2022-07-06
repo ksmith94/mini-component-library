1c555ffa8e35266eaf720d0c8febb5334b81d5df
Kevin Smith <102421938+ksmith94@users.noreply.github.com>
2022-07-06 07:50:58 -0400

    Build preliminary progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,44 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    '--height': 8 + 'px',
+    '--borderRadius': 4 + 'px',
+  },
+  medium: {
+    '--height': 12 + 'px',
+    '--borderRadius': 4 + 'px',
+  },
+  large: {
+    '--height': 24 + 'px',
+    '--borderRadius': 8 + 'px',
+    '--padding': 4 + 'px'
+  }
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+  return (
+    <Wrapper style={styles}>
+      <Progress value={value}></Progress>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 370px;
+  height: var(--height);
+  background-color: ${COLORS.transparentGray15};
+  border-radius: var(--borderRadius);
+  padding: var(--padding);
+`
+
+const Progress = styled.span`
+  background-color: blue;
+  width: 50px;
+  height: 10px;
+`
+
 
 export default ProgressBar;
